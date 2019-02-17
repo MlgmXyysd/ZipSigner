978065cd6f6e1d05b789b2b8db28fc3cda78f1b6
mlgmxyysd <1204074991@qq.com>
2019-02-17 15:52:25 +0800

    Initial commit

---
+++ app/src/main/java/org/meowcat/bugcatcher/MeowCatApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2013-2018 MeowCat Studio Powered by MlgmXyysd All Rights Reserved.
+ */
+
+package org.meowcat.bugcatcher;
+
+import android.app.Application;
+
+public class MeowCatApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+    }
+}
